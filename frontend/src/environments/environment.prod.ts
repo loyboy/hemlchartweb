25f8cfe4c5bb6af0063941c050d26ab4fb98512d
EC2 Default User <ec2-user@ip-172-31-13-247.ec2.internal>
2022-12-25 13:50:08 +0000

    second commit

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-prod.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
